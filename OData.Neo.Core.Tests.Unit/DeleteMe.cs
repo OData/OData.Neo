3f2cd5a61005ef8470eb85d2be8dbb5650c61a13
Hassan Habib <hassanhabib@live.com>
2022-01-24 13:24:48 -0800

    INFRA: Intialize OData.Neo.Core & OData.Neo.Core.Tests.Unit

---
+++ OData.Neo.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,16 @@
+//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using Xunit;
+
+namespace OData.Neo.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(true);
+    }
+}
