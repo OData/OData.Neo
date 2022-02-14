8f58aac6530317290822067122eaf72fed8494b1
Wardy <paul.ward@ccoder.co.uk>
2022-02-14 17:22:21 +0000

    ShouldTokenizeMultipleRawQueryWithNestedQuery -> PASS

--- OData.Neo.Core/Services/Foundations/Tokenizations/TokenizationService.cs
+++ OData.Neo.Core/Services/Foundations/Tokenizations/TokenizationService.cs
@@ -3,39 +3,44 @@
 // See License.txt in the project root for license information.
 //-----------------------------------------------------------------------
 
-using System.Collections.Generic;
-using System.Linq;
 using OData.Neo.Core.Models;
+using System.Linq;
 
 namespace OData.Neo.Core.Services.Foundations.Tokenizations
 {
     public class TokenizationService : ITokenizationService
     {
-        public ONode Tokenize(string rawQuery)
-        {
-            var items = rawQuery.Split('=');
-            var parameter = items[0];
-            var propertyChildren = items[1]
-                .Split(',')
-                .Select(s => new ONode {
-                    Type = ONodeType.Property,
-                    Value = s
-                })
-                .ToList();
-
-            var rootChildren = new List<ONode> {
-                new ONode {
-                    Type = ONodeType.Parameter,
-                    Value = parameter,
-                }
-            };
-
-            rootChildren.AddRange(propertyChildren);
-
-            return new ONode { 
+        public ONode Tokenize(string rawQuery) => new ONode
+            {
                 Type = ONodeType.Root,
-                Children = rootChildren
+                Children = ParseParameter(rawQuery)
             };
+
+        ONode[] ParseParameter(string query)
+        {
+            var items = query.Split('=')[0];
+            var parameter = query.Split('=')[0];
+            var properties = query.Split('=')[1].Split('(')[0].Split(',');
+
+            var children = query.Split('(').Length > 1
+                ? ParseParameter(query.Split('(')[1].TrimEnd(')'))
+                : null;
+            
+            var result = new[] {
+                new ONode
+                {
+                    Type = ONodeType.Parameter,
+                    Value = parameter
+                }
+            }.Union(properties.Select(p => new ONode
+                    {
+                        Type = ONodeType.Property,
+                        Value = p
+                    })
+                ).ToArray();
+
+            result.Last().Children = children;
+            return result;
         }
     }
 }
