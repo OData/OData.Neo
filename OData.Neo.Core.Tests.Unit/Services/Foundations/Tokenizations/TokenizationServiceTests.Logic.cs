3124a81b11c66a34355f5d8cf439e841596e8866
Wardy <paul.ward@ccoder.co.uk>
2022-02-16 18:08:15 +0000

    ShouldTokenizeMultipleRawQueryWithMultipleRandomlyNestedQueries -> FAIL

--- OData.Neo.Core.Tests.Unit/Services/Foundations/Tokenizations/TokenizationServiceTests.Logic.cs
+++ OData.Neo.Core.Tests.Unit/Services/Foundations/Tokenizations/TokenizationServiceTests.Logic.cs
@@ -6,69 +6,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using Newtonsoft.Json;
 using OData.Neo.Core.Models;
 using Xunit;
+
 
 namespace OData.Neo.Core.Tests.Unit.Services.Foundations.Tokenizations
 {
     public partial class TokenizationServiceTests
     {
-        [Fact]
-        public void ShouldTokenizeMultipleRawQueryWithNestedQuery()
-        {
-            var (inner, outer) = GetRandomQueryWithMultipleNestedProperties();
-
-            string query = $"{outer.Parameter}{outer.Operand}{string.Join(",", outer.Property)}" +
-                $"({inner.Parameter}{inner.Operand}{string.Join(",", inner.Property)})";
-
-            var rootChildren = new List<ONode> {
-                new ONode {
-                    Type = ONodeType.Parameter,
-                    Value = outer.Parameter
-                }};
-
-            var childrenNodes = outer.Property
-                .Select(property =>
-                    new ONode {
-                        Type = ONodeType.Property,
-                        Value = property
-                    });
-
-            rootChildren.AddRange(childrenNodes);
-
-            var lastChildRoot = new List<ONode> {
-                new ONode {
-                    Type = ONodeType.Parameter,
-                    Value = inner.Parameter
-                }};
-
-            var lastChildNodes = inner.Property
-                .Select(property =>
-                    new ONode {
-                        Type = ONodeType.Property,
-                        Value = property
-                    });
-
-            lastChildRoot.AddRange(lastChildNodes);
-
-            rootChildren.Last().Children = lastChildRoot;
-
-            var expectedNode = new ONode {
-                Type = ONodeType.Root,
-                Children = rootChildren
-            };
-
-            // when
-            ONode actualNode = this
-                .tokenizationService
-                .Tokenize(query);
-
-            // then
-            actualNode
-                .Should()
-                .BeEquivalentTo(expectedNode);
-        }
-
         [Fact]
         public void ShouldTokenizeRawQuery()
         {
@@ -143,6 +89,82 @@ namespace OData.Neo.Core.Tests.Unit.Services.Foundations.Tokenizations
 
             // then
             actualNode.Should().BeEquivalentTo(expectedNode);
+        }
+
+        [Fact]
+        public void ShouldTokenizeMultipleRawQueryWithNestedQuery()
+        {
+            var (inner, outer) = GetRandomQueryWithMultipleNestedProperties();
+
+            string query = $"{outer.Parameter}{outer.Operand}{string.Join(",", outer.Property)}" +
+                $"({inner.Parameter}{inner.Operand}{string.Join(",", inner.Property)})";
+
+            var rootChildren = new List<ONode> {
+                new ONode {
+                    Type = ONodeType.Parameter,
+                    Value = outer.Parameter
+                }};
+
+            var childrenNodes = outer.Property
+                .Select(property =>
+                    new ONode
+                    {
+                        Type = ONodeType.Property,
+                        Value = property
+                    });
+
+            rootChildren.AddRange(childrenNodes);
+
+            var lastChildRoot = new List<ONode> {
+                new ONode {
+                    Type = ONodeType.Parameter,
+                    Value = inner.Parameter
+                }};
+
+            var lastChildNodes = inner.Property
+                .Select(property =>
+                    new ONode
+                    {
+                        Type = ONodeType.Property,
+                        Value = property
+                    });
+
+            lastChildRoot.AddRange(lastChildNodes);
+
+            rootChildren.Last().Children = lastChildRoot;
+
+            var expectedNode = new ONode
+            {
+                Type = ONodeType.Root,
+                Children = rootChildren
+            };
+
+            // when
+            ONode actualNode = this
+                .tokenizationService
+                .Tokenize(query);
+
+            // then
+            actualNode
+                .Should()
+                .BeEquivalentTo(expectedNode);
+        }
+
+        [Fact]
+        public void ShouldTokenizeMultipleRawQueryWithMultipleRandomlyNestedQueries()
+        {
+            // given
+            var model = GetRandomQueryModel();
+            var modelJson = ToJson(model);
+            var query = ToQueryString(model);
+
+            // when
+            var result = tokenizationService.Tokenize(query);
+
+            // then
+            result.Should().BeEquivalentTo(model);
+            ToJson(result).Should().BeEquivalentTo(modelJson);
+            ToQueryString(result).Should().BeEquivalentTo(query);
         }
     }
 }
--- OData.Neo.Core.Tests.Unit/Services/Foundations/Tokenizations/TokenizationServiceTests.cs
+++ OData.Neo.Core.Tests.Unit/Services/Foundations/Tokenizations/TokenizationServiceTests.cs
@@ -3,7 +3,10 @@
 // See License.txt in the project root for license information.
 //-----------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using OData.Neo.Core.Models;
 using OData.Neo.Core.Services.Foundations.Tokenizations;
 using Tynamix.ObjectFiller;
 
@@ -13,30 +16,22 @@ namespace OData.Neo.Core.Tests.Unit.Services.Foundations.Tokenizations
     {
         private readonly ITokenizationService tokenizationService;
 
-        public TokenizationServiceTests() =>
-            this.tokenizationService = new TokenizationService();
+        public TokenizationServiceTests() => tokenizationService = new TokenizationService();
 
-        public static (string Parameter, string Operand, string Property)
-            GetRandomQueryParameters()
+        public static (string Parameter, string Operand, string Property) GetRandomQueryParameters()
         {
-            string randomParameter =
-                $"${new MnemonicString().GetValue()}";
-
-            string randomProperty =
-                new MnemonicString().GetValue();
-
+            string randomParameter = $"${new MnemonicString().GetValue()}";
+            string randomProperty = new MnemonicString().GetValue();
             return (randomParameter, "=", randomProperty);
         }
 
-        public static (string Parameter, string Operand, string[] Property)
-            GetRandomQueryWithMultipleProperties()
+        public static (string Parameter, string Operand, string[] Property) GetRandomQueryWithMultipleProperties()
         {
-            string randomParameter =
-                $"${new MnemonicString().GetValue()}";
+            string randomParameter = $"${new MnemonicString().GetValue()}";
 
-            string[] randomProperties =
-                Enumerable.Range(start: 0, count: GetRandomNumber())
-                    .Select(item => new MnemonicString().GetValue()).ToArray();
+            string[] randomProperties = Enumerable.Range(start: 0, count: GetRandomNumber())
+                    .Select(item => new MnemonicString().GetValue())
+                    .ToArray();
 
             return (randomParameter, "=", randomProperties);
         }
@@ -50,7 +45,48 @@ namespace OData.Neo.Core.Tests.Unit.Services.Foundations.Tokenizations
                 GetRandomQueryWithMultipleProperties()
             );
 
-        private static int GetRandomNumber() =>
-            new IntRange(min: 2, max: 10).GetValue();
+        public static ONode GetRandomQueryModel() => new()
+        {
+            Type = ONodeType.Root,
+            Children = GetRandomONodeArray()
+        };
+
+        public static ONode[] GetRandomONodeArray()
+        {
+            var rootChildren = BuildChildrenForQueryPart(GetRandomQueryWithMultipleProperties());
+            rootChildren
+                .Skip(1)
+                .Take(GetRandomNumber())
+                .Last()
+                .Children = GetRandomNumber() > 3
+                    ? GetRandomONodeArray()
+                    : null;
+
+            return rootChildren.ToArray();
+        }
+
+        public static ONode[] BuildChildrenForQueryPart((string Parameter, string Operand, string[] Property) part) 
+            => new List<ONode> { new ONode { Type = ONodeType.Parameter, Value = part.Parameter } }
+                .Union(part.Property
+                    .Select(property => new ONode { Type = ONodeType.Property, Value = property }))
+                    .ToArray();
+
+        private static int GetRandomNumber() => new IntRange(min: 2, max: 10).GetValue();
+
+        public string ToJson(ONode source)
+        {
+            var children = source.Children != null
+                ? $"[{string.Join(",", source.Children.Select(c => c.ToJson()))}]"
+                : null;
+
+            return children != null
+                ? $"{{ \"Type\": \"{source.Type}\", \"Value\": \"{source.Value}\" \"Children\": {children} }}"
+                : $"{{ \"Type\": \"{source.Type}\", \"Value\": \"{source.Value}\" }}";
+        }
+
+        public string ToQueryString(ONode source)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
