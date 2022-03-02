b45f12dfad7a299679d0841bf11d9a77a6e460e6
Joe LaFountain <josuave83@gmail.com>
2022-03-02 13:00:14 -0500

    ShouldTokenizeQuery -> PASS

--- OData.Neo.Core/Services/Foundations/Tokenizations/TokenizationService.cs
+++ OData.Neo.Core/Services/Foundations/Tokenizations/TokenizationService.cs
@@ -3,7 +3,6 @@
 // See License.txt in the project root for license information.
 //-----------------------------------------------------------------------
 
-using System;
 using OData.Neo.Core.Models;
 
 namespace OData.Neo.Core.Services.Foundations.Tokenizations
@@ -25,11 +24,107 @@ namespace OData.Neo.Core.Services.Foundations.Tokenizations
                         Type = OTokenType.Operand,
                         Value = rawQuery
                     },
+                    "gt" => new OToken
+                    {
+                        Type = OTokenType.Operand,
+                        Value = rawQuery
+                    },
+                    "lt" => new OToken
+                    {
+                        Type = OTokenType.Operand,
+                        Value = rawQuery
+                    },
+                    "le" => new OToken
+                    {
+                        Type = OTokenType.Operand,
+                        Value = rawQuery
+                    },
+
                     " " => new OToken
                     {
                         Type = OTokenType.Whitespace,
                         Value = rawQuery
                     },
+                    "(" => new OToken
+                    {
+                        Type = OTokenType.BeginScope,
+                        Value = rawQuery
+                    },
+                    ")" => new OToken
+                    {
+                        Type = OTokenType.EndScope,
+                        Value = rawQuery
+                    },
+                    "=" => new OToken
+                    {
+                        Type = OTokenType.Equals,
+                        Value = rawQuery
+                    },
+                    "-" => new OToken
+                    {
+                        Type = OTokenType.Hyphen,
+                        Value = rawQuery
+                    },
+                    "." => new OToken
+                    {
+                        Type = OTokenType.Dot,
+                        Value = rawQuery
+                    },
+                    "'" => new OToken
+                    {
+                        Type = OTokenType.Quote,
+                        Value = rawQuery
+                    },
+                    "*" => new OToken
+                    {
+                        Type = OTokenType.Star,
+                        Value = rawQuery
+                    },
+                    "[" => new OToken
+                    {
+                        Type = OTokenType.OpenBracket,
+                        Value = rawQuery
+                    },
+                    "]" => new OToken
+                    {
+                        Type = OTokenType.CloseBracket,
+                        Value = rawQuery
+                    },
+                    ":" => new OToken
+                    {
+                        Type = OTokenType.Colon,
+                        Value = rawQuery
+                    },
+                    ";" => new OToken
+                    {
+                        Type = OTokenType.SemiColon,
+                        Value = rawQuery
+                    },
+                    "_" => new OToken
+                    {
+                        Type = OTokenType.Underscore,
+                        Value = rawQuery
+                    },
+                    "&" => new OToken
+                    {
+                        Type = OTokenType.Ampersand,
+                        Value = rawQuery
+                    },
+                    "$" => new OToken
+                    {
+                        Type = OTokenType.Dollar,
+                        Value = rawQuery
+                    },
+                    "/" => new OToken
+                    {
+                        Type = OTokenType.Slash,
+                        Value = rawQuery
+                    },
+                    "\\" => new OToken
+                    {
+                        Type = OTokenType.BackSlash,
+                        Value = rawQuery
+                    }
                 }
             };
         }
