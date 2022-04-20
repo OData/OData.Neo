a4bf6a0688e80e1e4f416cee37df83307401bfe9
Hassan Habib <hassanhabib@live.com>
2022-04-20 09:54:09 -0700

    ShouldThrowValidationExceptionOnTokenizeIfQueryIsNull -> FAIL

---
+++ OData.Neo.Core.Tests.Unit/Services/Foundations/Tokenizations/TokenizationServiceTests.Validations.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using System;
+using OData.Neo.Core.Models.OTokens.Exceptions;
+using Xunit;
+
+
+namespace OData.Neo.Core.Tests.Unit.Services.Foundations.Tokenizations
+{
+    public partial class TokenizationServiceTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnTokenizeIfQueryIsNull()
+        {
+            // given
+            string nullQuery = null;
+
+            // when
+            Action toknizeAction = () =>
+                this.tokenizationService.Tokenize(nullQuery);
+
+            // then
+            Assert.Throws<OTokenValidationException>(toknizeAction);
+        }
+    }
+}
