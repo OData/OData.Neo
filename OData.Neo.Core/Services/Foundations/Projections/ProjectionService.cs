671b211daf922ae490f079bb952d5ad4ce4cdfc3
Joe <josuave83@outlook.com>
2022-05-13 13:34:27 -0400

    ShouldProjectAllTokensKeywords -> PASS

--- OData.Neo.Core/Services/Foundations/Projections/ProjectionService.cs
+++ OData.Neo.Core/Services/Foundations/Projections/ProjectionService.cs
@@ -3,8 +3,6 @@
 // See License.txt in the project root for license information.
 //-----------------------------------------------------------------------
 
-using System;
-using System.Linq;
 using OData.Neo.Core.Models.ProjectedTokens;
 
 namespace OData.Neo.Core.Services.Foundations.Projections
@@ -13,7 +11,10 @@ namespace OData.Neo.Core.Services.Foundations.Projections
     {
         public ProjectedToken[] ProjectTokens(ProjectedToken[] projectedTokens)
         {
-            projectedTokens[0].ProjectedType = ProjectedType.Keyword;
+            foreach (var projectedToken in projectedTokens)
+            {
+                projectedToken.ProjectedType = ProjectedType.Keyword;
+            }
 
             return projectedTokens;
         }
