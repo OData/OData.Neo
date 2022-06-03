a44716ed8a07c3fc2be97330fc76229a52af83da
Hassan Habib <hassanhabib@live.com>
2022-06-03 08:35:07 -0700

    ShouldThrowValidationExceptionIfAnyProjectedTokenIsNull -> PASS

--- OData.Neo.Core/Services/Foundations/Projections/ProjectionService.Validations.cs
+++ OData.Neo.Core/Services/Foundations/Projections/ProjectionService.Validations.cs
@@ -5,6 +5,7 @@
 
 using OData.Neo.Core.Models.ProjectedTokens;
 using OData.Neo.Core.Models.ProjectedTokens.Exceptions;
+using System.Linq;
 
 namespace OData.Neo.Core.Services.Foundations.Projections
 {
@@ -12,7 +13,23 @@ namespace OData.Neo.Core.Services.Foundations.Projections
     {
         private void ValidateProjectedTokens(ProjectedToken[] projectedTokens)
         {
-            if(projectedTokens is null)
+            ValidateProjectedTokensIsNotNull(projectedTokens);
+            ValidateProjectedTokensContainsNotNullTokens(projectedTokens);
+        }
+
+        private static void ValidateProjectedTokensIsNotNull(
+            ProjectedToken[] projectedTokens)
+        {
+            if (projectedTokens is null)
+            {
+                throw new NullProjectedTokenException();
+            }
+        }
+
+        private static void ValidateProjectedTokensContainsNotNullTokens(
+            ProjectedToken[] projectedTokens)
+        {
+            if (projectedTokens.Contains(null))
             {
                 throw new NullProjectedTokenException();
             }
