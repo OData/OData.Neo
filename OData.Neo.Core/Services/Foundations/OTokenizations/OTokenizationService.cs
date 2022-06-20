a60bedeed6b2c822acc7d44e5cf0b6ed025e2204
Joe <josuave83@outlook.com>
2022-06-20 11:23:33 -0400

    ShouldOTokenize -> PASS

--- OData.Neo.Core/Services/Foundations/OTokenizations/OTokenizationService.cs
+++ OData.Neo.Core/Services/Foundations/OTokenizations/OTokenizationService.cs
@@ -3,14 +3,40 @@
 // See License.txt in the project root for license information.
 //-----------------------------------------------------------------------
 
-using System;
 using OData.Neo.Core.Models.OTokens;
+using OData.Neo.Core.Models.ProjectedTokens;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OData.Neo.Core.Services.Foundations.OTokenizations
 {
     public class OTokenizationService : IOTokenizationService
     {
-        public OToken OTokenize(OToken[] oTokens) =>
-            throw new NotImplementedException();
+        public OToken OTokenize(OToken[] oTokens)
+        {
+            OToken root = new OToken
+            {
+                Type = OTokenType.Root,
+                Children = new List<OToken>()
+            };
+
+            var selectToken = oTokens[0];
+            selectToken.Type = OTokenType.Select;
+
+            root.Children.Add(selectToken);
+            selectToken.Children ??= new();
+            var tokens = oTokens
+                .Skip(1)
+                .Where(token => token.ProjectedType != ProjectedTokenType.Equals)
+                .Select(token =>
+                {
+                    token.Type = OTokenType.Property;
+                    return token;
+                });
+
+            selectToken.Children.AddRange(tokens);
+
+            return root;
+        }
     }
 }
