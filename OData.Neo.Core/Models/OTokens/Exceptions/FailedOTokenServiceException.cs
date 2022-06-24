d578f94affcd96f2446baedbc51496143105ab98
Hassan Habib <hassanhabib@live.com>
2022-06-24 08:59:07 -0700

    ShouldThrowServiceExceptionOnTokenizationIfServiceErrorOccurs -> FAIL

---
+++ OData.Neo.Core/Models/OTokens/Exceptions/FailedOTokenServiceException.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace OData.Neo.Core.Models.OTokens.Exceptions
+{
+    public class FailedOTokenServiceException : Xeption
+    {
+        public FailedOTokenServiceException(Exception innerException)
+            : base(message: "Failed otoken service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
---
+++ OData.Neo.Core/Models/OTokens/Exceptions/OTokenServiceException.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using Xeptions;
+
+namespace OData.Neo.Core.Models.OTokens.Exceptions
+{
+    public class OTokenServiceException : Xeption
+    {
+        public OTokenServiceException(Xeption innerException)
+            : base(message: "OToken service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
