e29dacf0bdf7723a0e2eaef9c4e91a683d02f715
Hassan Habib <hassanhabib@live.com>
2022-08-26 08:59:00 -0700

    BROKERS: Finalize Generate Scripts

--- OData.Neo.Core/Brokers/Expressions/ExpressionBroker.cs
+++ OData.Neo.Core/Brokers/Expressions/ExpressionBroker.cs
@@ -3,13 +3,13 @@
 // See License.txt in the project root for license information.
 //-----------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
+using OData.Neo.Core.Models.Expressions;
 
 namespace OData.Neo.Core.Brokers.Expressions
 {
@@ -17,35 +17,46 @@ namespace OData.Neo.Core.Brokers.Expressions
     {
         public async ValueTask<Expression> GenerateExpressionAsync<T>(string linqExpression)
         {
-            var globals = new Globals<Student>
-            {
-                DataSource = new List<Student>().AsQueryable()
-            };
+            Globals<T> globals = GetGlobalVariables<T>();
+            ScriptOptions scriptOptions = GetScriptOptions<T>();
+            string script = GetScript(linqExpression);
 
+            ScriptState<Expression> state =
+                await CSharpScript.RunAsync<Expression>(
+                    code: script,
+                    scriptOptions,
+                    globals);
+
+            return state.ReturnValue;
+        }
+
+        protected virtual Globals<T> GetGlobalVariables<T>()
+        {
+            return new Globals<T>
+            {
+                DataSource = new List<T>().AsQueryable()
+            };
+        }
+
+        protected virtual ScriptOptions GetScriptOptions<T>()
+        {
             ScriptOptions scriptOptions = ScriptOptions.Default;
             scriptOptions = scriptOptions.AddReferences("System");
             scriptOptions = scriptOptions.AddReferences("System.Linq");
             scriptOptions = scriptOptions.AddReferences("System.Collections.Generic");
+            scriptOptions = scriptOptions.AddReferences(typeof(T).Assembly);
 
-            var state = await CSharpScript.RunAsync<Expression>($@"
+            return scriptOptions;
+        }
+
+        protected virtual string GetScript(string linqExpression)
+        {
+            return $@"
                 using System;
                 using System.Linq;
                 using System.Collections.Generic;
-                
-                return DataSource.{linqExpression}.Expression;", scriptOptions, globals);
 
-            return state.ReturnValue;
-        }
-        public class Globals<T>
-        {
-            public IQueryable<T> DataSource { get; set; }
-        }
-
-        public class Student
-        {
-            public string Name { get; set; }
-            public Guid Id { get; set; }
+                return DataSource.{linqExpression}.Expression;";
         }
     }
-
 }
---
+++ OData.Neo.Core/Models/Expressions/Globals.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using System;
+using System.Linq;
+
+namespace OData.Neo.Core.Models.Expressions
+{
+    public class Globals<T>
+    {
+        public IQueryable<T> DataSource { get; set; }
+    }
+}
