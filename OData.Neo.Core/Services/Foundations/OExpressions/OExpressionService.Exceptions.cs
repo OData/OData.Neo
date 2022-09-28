9eebbf1cd8dda3db3fdd2a3c526f5932fcba3853
Sam Xu <saxu@microsoft.com>
2022-09-28 10:58:13 -0700

    ShouldThrowDependencyExceptionOnGenerateIfDependencyErrorOcurrsAndLogitAsync -> PASS

--- OData.Neo.Core/Services/Foundations/OExpressions/OExpressionService.Exceptions.cs
+++ OData.Neo.Core/Services/Foundations/OExpressions/OExpressionService.Exceptions.cs
@@ -3,7 +3,9 @@
 // See License.txt in the project root for license information.
 //-----------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Scripting;
 using OData.Neo.Core.Models.OExpressions;
 using OData.Neo.Core.Models.OExpressions.Exceptions;
 
@@ -27,6 +29,27 @@ namespace OData.Neo.Core.Services.Foundations.OExpressions
             catch (InvalidOExpressionException invalidOExpressionException)
             {
                 throw new OExpressionValidationException(invalidOExpressionException);
+            }
+            catch (ArgumentNullException argumentNullException)
+            {
+                var failedOExpressionDependencyException =
+                    new FailedOExpressionDependencyException(argumentNullException);
+
+                throw new OExpressionDependencyException(failedOExpressionDependencyException);
+            }
+            catch (ArgumentException argumentException)
+            {
+                var failedOExpressionDependencyException =
+                    new FailedOExpressionDependencyException(argumentException);
+
+                throw new OExpressionDependencyException(new FailedOExpressionDependencyException(argumentException));
+            }
+            catch (CompilationErrorException compilcationErrorException)
+            {
+                var failedOExpressionDependencyException =
+                    new FailedOExpressionDependencyException(compilcationErrorException);
+
+                throw new OExpressionDependencyException(failedOExpressionDependencyException);
             }
         }
     }
