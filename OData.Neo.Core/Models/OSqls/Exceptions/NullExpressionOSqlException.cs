2e7385fa7e8161e6d9cc777ad073acbf84459f05
Hassan Habib <hassanhabib@live.com>
2022-11-09 08:51:26 -0800

    ShouldThrowValidationExceptionOnRetrieveIfExpressionIsNull -> FAIL

---
+++ OData.Neo.Core/Models/OSqls/Exceptions/NullExpressionOSqlException.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using Xeptions;
+
+namespace OData.Neo.Core.Models.OSqls.Exceptions
+{
+    public class NullExpressionOSqlException : Xeption
+    {
+        public NullExpressionOSqlException()
+            : base(message: "Null expression OSQL error ocurred, fix the erros and try again.")
+        { }
+    }
+}
