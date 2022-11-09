e4f641c9b9c5caccebad96c995075e2bf319106c
Hassan Habib <hassanhabib@live.com>
2022-11-09 09:17:45 -0800

    ShouldThrowDependencyExceptionOnRetrieveIfDependencyErrorOccurs -> FAIL

---
+++ OData.Neo.Core.Tests.Unit/Services/Foundations/OSqls/OSqlServiceTests.Exceptions.Retrieve.cs
@@ -0,0 +1,57 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using FluentAssertions;
+using Moq;
+using OData.Neo.Core.Models.OSqls.Exceptions;
+using Xunit;
+
+namespace OData.Neo.Core.Tests.Unit.Services.Foundations.OSqls
+{
+    public partial class OSqlServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public void ShouldThrowDependencyExceptionOnRetrieveIfDependencyErrorOccurs(
+            Exception dependencyException)
+        {
+            // given
+            Expression someExpression = CreateMockedExpression();
+
+            var failedOSqlDependencyException =
+                new FailedOSqlDependencyException(
+                    dependencyException);
+
+            var expectedOSqlDependencyException =
+                new OSqlDependencyException(
+                    failedOSqlDependencyException);
+
+            this.sqlQueryBrokerMock.Setup(broker =>
+                broker.GetSqlQuery(It.IsAny<Expression>()))
+                    .Throws(dependencyException);
+
+            // when
+            Action getOSqlAction = () =>
+                this.oSqlService.RetrieveOSqlQuery(
+                    someExpression);
+
+            OSqlDependencyException actualOSqlDependencyException =
+                Assert.Throws<OSqlDependencyException>(
+                    getOSqlAction);
+
+            // then
+            actualOSqlDependencyException.Should().BeEquivalentTo(
+                expectedOSqlDependencyException);
+
+            this.sqlQueryBrokerMock.Verify(broker =>
+                broker.GetSqlQuery(It.IsAny<Expression>()),
+                    Times.Once);
+
+            this.sqlQueryBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- OData.Neo.Core.Tests.Unit/Services/Foundations/OSqls/OSqlServiceTests.cs
+++ OData.Neo.Core.Tests.Unit/Services/Foundations/OSqls/OSqlServiceTests.cs
@@ -3,11 +3,13 @@
 // See License.txt in the project root for license information.
 //-----------------------------------------------------------------------
 
+using System;
 using System.Linq.Expressions;
 using Moq;
 using OData.Neo.Core.Brokers.Queries;
 using OData.Neo.Core.Services.Foundations.OSqls;
 using Tynamix.ObjectFiller;
+using Xunit;
 
 namespace OData.Neo.Core.Tests.Unit.Services.Foundations.OSqls
 {
@@ -23,6 +25,15 @@ namespace OData.Neo.Core.Tests.Unit.Services.Foundations.OSqls
 
             this.oSqlService = new OSqlService(
                 this.sqlQueryBrokerMock.Object);
+        }
+
+        public static TheoryData DependencyExceptions()
+        {
+            return new TheoryData<Exception>
+            {
+                new InvalidOperationException(),
+                new InvalidCastException()
+            };
         }
 
         private static string GetRandomSqlQuery() =>
