4ef7fb1a75b703ca3a308bda397980464e3a3476
Hassan Habib <hassanhabib@live.com>
2023-02-06 12:17:15 -0800

    ShouldProcessODataTokenAsync -> FAIL

---
+++ OData.Neo.Core.Tests.Unit/Services/Coordinations/OQueries/OQueryCoordinationServiceTests.Logic.Process.cs
@@ -0,0 +1,73 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using OData.Neo.Core.Models.OExpressions;
+using OData.Neo.Core.Models.OTokens;
+using OData.Neo.Core.Services.Coordinations.OQueries;
+using OData.Neo.Core.Services.Orchestrations.OQueries;
+using OData.Neo.Core.Services.Orchestrations.OTokenizations;
+using Xunit;
+
+namespace OData.Neo.Core.Tests.Unit.Services.Coordinations.OQueries
+{
+    public partial class OQueryCoordinationServiceTests
+    {
+        [Fact]
+        public async Task ShouldProcessODataTokenAsync()
+        {
+            // given
+            string randomODataQuery = GetRandomODataQuery();
+            string inputODataQuery = randomODataQuery;
+            OToken randomOToken = CreateRandomOToken();
+            OToken tokenizedQuery = randomOToken;
+
+            var expectedInputOExpression = new OExpression
+            {
+                OToken = tokenizedQuery,
+                RawQuery = inputODataQuery
+            };
+
+            Expression randomExpression = CreateMockedExpression();
+            Expression expectedExpression = randomExpression;
+
+            var processedOExpression = new OExpression();
+            processedOExpression.Expression = randomExpression;
+
+            this.oTokenizationOrchestrationServiceMock.Setup(service =>
+                service.OTokenizeQuery(inputODataQuery))
+                    .Returns(tokenizedQuery);
+
+            this.oQueryOrchestrationServiceMock.Setup(service =>
+                service.ProcessOQueryAsync<object>(It.Is(SameOExpressionAs(
+                    expectedInputOExpression))))
+                        .ReturnsAsync(processedOExpression);
+
+            // when
+            Expression actualExpression =
+                await this.oQueryCoordinationService.ProcessOQueryAsync<object>(
+                    inputODataQuery);
+
+            // then
+            actualExpression.Should().BeEquivalentTo(expectedExpression);
+
+            this.oTokenizationOrchestrationServiceMock.Verify(service =>
+                service.OTokenizeQuery(inputODataQuery),
+                    Times.Once);
+
+            this.oQueryOrchestrationServiceMock.Verify(service =>
+                service.ProcessOQueryAsync<object>(It.Is(SameOExpressionAs(
+                    expectedInputOExpression))),
+                        Times.Once);
+
+            this.oQueryOrchestrationServiceMock.VerifyNoOtherCalls();
+            this.oTokenizationOrchestrationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ OData.Neo.Core/Services/Coordinations/OQueries/OQueryCoordinationService.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using OData.Neo.Core.Services.Orchestrations.OQueries;
+using OData.Neo.Core.Services.Orchestrations.OTokenizations;
+
+namespace OData.Neo.Core.Services.Coordinations.OQueries
+{
+    public partial class OQueryCoordinationService : IOQueryCoordinationService
+    {
+        private readonly IOTokenizationOrchestrationService oTokenizationOrchestrationService;
+        private readonly IOQueryOrchestrationService oQueryOrchestrationService;
+
+        public OQueryCoordinationService(
+            IOTokenizationOrchestrationService oTokenizationOrchestrationService,
+            IOQueryOrchestrationService oQueryOrchestrationService)
+        {
+            this.oTokenizationOrchestrationService = oTokenizationOrchestrationService;
+            this.oQueryOrchestrationService = oQueryOrchestrationService;
+        }
+
+        public ValueTask<Expression> ProcessOQueryAsync<T>(string oQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
